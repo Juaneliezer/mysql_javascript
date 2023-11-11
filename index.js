68c91770fcfdbe044a3726685642ce9157da40a2
Juaneliezer <jeliezer26@hotmail.com>
2023-11-11 19:50:22 -0400

    actualizacion del index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql =require('mysql');
 
-const app =express();
+const app = express();
 
 const connection = mysql.createConnection({
     host:'localhost',
@@ -11,13 +11,19 @@ const connection = mysql.createConnection({
 
 });
 
-app.set("view engine", "ejs");
-
-app.listen(8000, function(){
-    console.log("servidor https://localhost:8000");
+connection.connect( (err) =>{
+    if(err) throw err
+    console.log('base de datos conectada')
 });
 
-app.use(express.json);
+
+app.set("view engine", "ejs");
+
+app.listen(3200, function(){
+    console.log("servidor http://localhost:3200");
+});
+
+app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.get("/", function(req,res){
@@ -28,27 +34,21 @@ app.post("/validar",function(req,res){
     const datos= req.body;
 
     let nom= datos.nom;
-    let num= datos.num;
+    let tel= datos.tel;
     let ced= datos.ced;
     
-    let registrar ="INSERT INTO usuarios (nombre, numero, cedula) VALUES('"+nom+"','"+num+"','"+ced+"')";
+    let registrar ="INSERT INTO usuarios (nombre, numero, cedula) VALUES('"+nom+"','"+tel+"','"+ced+"')";
 
     connection.query(registrar,function(error){
         if(error){
             throw error;
 
         }else{
-            console.log('Datos almacenados')
+            console.log('Datos almacenados');
         }
     });
 
 
-});
-
-
-connection.connect( (err) =>{
-    if(err) throw err
-    console.log('base de datos conectada')
 });
 
 
